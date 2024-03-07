95258ad1b6688a8b2860d2ae4cba705ee751b861
ThibeHanssens <thibe.hanssens@outlook.com>
2024-03-07 08:59:36 +0100

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -43,8 +42,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    public function chirps(): HasMany
-    {
-        return $this->hasMany(Chirp::class);
-    }
 }
